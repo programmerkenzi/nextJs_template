679acefa3ba5fbb3999b5f608ed3745daaeaeaa7
kenzi <a23g32@gmail.com>
2021-11-03 16:08:28 +0800

    added react redux toolkit

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
+import store from "../store";
+import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <Provider store={store}>
+      <Component {...pageProps} />;
+    </Provider>
   );
 }
 
